fdcc496b6e6af40984be0ee0d2126d457e06fffc
Bshilpa2610 <45521000+Bshilpa2610@users.noreply.github.com>
2022-08-24 13:33:26 +0530

    Add files via upload

---
+++ ADV-C98-student/Voice_selfie_app/main.js
@@ -0,0 +1,32 @@
+var SR = window.webkitSpeechRecognition;
+var speech = new SR()
+
+function start(){
+    document.getElementById('textbox').innerHTML = ''
+    speech.start()
+};
+
+function speak(){
+    var synth = window.speechSynthesis
+    speakData = document.getElementById('textbox').value;
+    var utterThis = new SpeechSynthesisUtterance(speakData)
+    synth.speak(utterThis)
+    Webcam.attach(camera)
+}
+
+camera = document.getElementById('camera')
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: 'png',
+    png_quality: 90
+})
+
+
+speech.onresult = function(event){
+    console.log(event)
+    var Content = event.results[0][0].transcript
+    console.log(Content)
+    document.getElementById('textbox').innerHTML = Content
+    speak()
+}
